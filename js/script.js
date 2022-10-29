819b4ca1f18fc936c1e893c6709c6c238fcc61ff
Rafał Siepak <rafal.siepak@outlook.com>
2022-10-29 14:22:26 +0200

    changes in script

--- js/script.js
+++ js/script.js
@@ -1,41 +1,33 @@
-
-
-{
-    const welcome = () => {
-        console.log("Witam serdecznie wszystkich!");
+const welcome = () => {
+    console.log("Witam serdecznie wszystkich!");
+  };
+  
+  const onChangeHeaderClick = () => {
+    const header__subHeader = document.querySelector(".header__subHeader");
+    header__subHeader.classList.toggle("header__subHeaderSpecial");
+  };
+  const toggleHeader = () => {
+    let header__subHeader = document.querySelector(".header__subHeader");
+    header__subHeader.classList.toggle("header__subHeaderSpecial");
+  };
+  const toggleBackground = () => {
+    const body = document.querySelector(".body");
+    const themeName = document.querySelector(".js-themeName");
+    body.classList.toggle("js-dark");
+  
+    if (body.classList.contains("js-dark")) {
+      themeName.innerText = "jasny";
+    } else {
+      themeName.innerText = "ciemny";
     }
-
-<<<<<<< HEAD
-    const onChangeHeaderClick = () => {
-        const header__subHeader = document.querySelector(".header__subHeader");
-        header__subHeader.classList.toggle("header__subHeaderSpecial");
-=======
-    const toggleHeader = () => {
-        let header__subHeader = document.querySelector(".header__subHeader")
-        header__subHeader.classList.toggle("header__subHeaderSpecial")
->>>>>>> ffd9cb7c6a051ab9a5ce6423ca78e870f28d97a1
-
-    const toggleBackground = () => {
-        const body = document.querySelector(".body");
-        const themeName = document.querySelector(".js-themeName");
-        body.classList.toggle("js-dark");
-
-        if (body.classList.contains("js-dark")) {
-            themeName.innerText = "jasny";
-        } else {
-            themeName.innerText = "ciemny";
-        }
-    };
-
-    const init = () => {
-        const button = document.querySelector(".js-button");
-        button.addEventListener("click", toggleBackground);
-        const press = document.querySelector(".js-press");
-        press.addEventListener("click", toggleHeader);
-        welcome();
-
-    };
-
-    init();
-
-};
+  };
+  
+  const init = () => {
+    const button = document.querySelector(".js-button");
+    button.addEventListener("click", toggleBackground);
+    const press = document.querySelector(".js-press");
+    press.addEventListener("click", toggleHeader);
+    welcome();
+  };
+  
+  init();
